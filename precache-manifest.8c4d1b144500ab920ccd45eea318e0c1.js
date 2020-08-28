ca97c2d66f8df79abb37b92c103f67b41d988223
Karol Wąsik <karol.wasik00@gmail.com>
2020-08-28 21:32:25 +0200

    Updates

---
+++ precache-manifest.8c4d1b144500ab920ccd45eea318e0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a971494d28a740a410caddf4028b83fb",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "348131c7333737df9815",
+    "url": "/ToDoList/static/css/main.1cad79fd.chunk.css"
+  },
+  {
+    "revision": "b588210e20582e987daf",
+    "url": "/ToDoList/static/js/2.2893b450.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.2893b450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348131c7333737df9815",
+    "url": "/ToDoList/static/js/main.99c1da1f.chunk.js"
+  },
+  {
+    "revision": "287390a9107f968645db",
+    "url": "/ToDoList/static/js/runtime-main.b3a0683f.js"
+  }
+]);
